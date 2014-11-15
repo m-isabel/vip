4c48547f4e2035ea130ac15c0f3d8d8beead8431
Vipul Nataraj <vnataraj@purdue.edu>
2014-11-15 01:39:49 -0500

    added js stuff

---
+++ js/sos_parse.js
@@ -0,0 +1,51 @@
+function create(username, password, email, firstname, lastname, phone){
+  var user = new Parse.User();
+  user.set("username", username);
+  user.set("password", password);
+  user.set("email", email);
+  user.set("firstname", firstname);
+  user.set("lastname", lastname);
+  user.set("phone", phone);
+  user.signUp(null, {
+    success: function(user){
+      $('#btn-signup').submit();
+    }, error: function(user, error){
+      alert("Error: " + error.code + " " + error.message);
+    }
+  });
+};
+
+function login(username, password){
+  Parse.User.logIn(username, password, {
+    success: function(user){
+       $('#btn-login').submit();
+    }, error: function(user, error){
+        alert("Error: " + error.code + " " + error.message);
+    }
+    });
+};
+
+function loginManager(func, callback){
+  setTimeout(function(){
+    func();
+    if(callback){callback();}}, 5400000);
+
+};
+
+function logout(){
+  Parse.User.logOut();
+  var currentUser = Parse.User.current();
+}
+
+/*
+$(window).load(function() {
+  Parse.initialize("");
+}
+
+
+$('#btn-login').click(function(e){
+  var user = $('#user').val();
+  var password = $('#pass').val();
+  login(user, pass);
+});
+*/
